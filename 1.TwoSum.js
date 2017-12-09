8212f354aa679d4b62327923145798f8aa39f014
dream510 <825473975@qq.com>
2017-12-10 01:15:04 +0800

    1

---
+++ 1.TwoSum.js
@@ -0,0 +1,24 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var arrResult=[];
+    var numLength=nums.lenght;
+    for(var i=0;i<numLength;i++){
+        for(var j=i+1;j<numLength;j++){
+            var sum=nums[i]+nums[j];
+            if(sum===target){
+                arrResult.push(i,j);
+               break;
+            }
+        }
+    }
+    return arrResult;
+};
